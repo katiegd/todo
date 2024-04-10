4f0ccc1f7a97bbda428d99211ec9e9a0d2d8c0f9
Katie Duryea <155897343+katiegd@users.noreply.github.com>
2024-04-10 16:14:38 -0400

    working on JS functionality. trying to decide how to organize via modules, classes, etc...

---
+++ index.js
@@ -0,0 +1,45 @@
+const addProjectBtn = document.querySelector("#add-project");
+const projectPanel = document.querySelector("#project-panel");
+const projectNameInput = document.querySelector("#project-name-input");
+const projectList = document.querySelector("#project-list");
+const deleteProjectBtn = document.querySelector("#project-delete");
+
+addProjectBtn.addEventListener("click", addProject);
+deleteProjectBtn.addEventListener("click", deleteProject);
+
+function addProject() {
+  const projectItem = document.createElement("div");
+  projectItem.setAttribute("id", "project-item");
+
+  const projectName = document.createElement("div");
+  projectName.textContent = projectNameInput.value;
+  projectName.setAttribute("id", "project-item-name");
+
+  const projectEditDeleteDiv = document.createElement("div");
+  projectEditDeleteDiv.setAttribute("id", "edit-delete");
+
+  const projectEdit = document.createElement("img");
+  projectEdit.setAttribute("id", "project-edit");
+  projectEdit.src = "./assets/edit.svg";
+  projectEdit.width = "25";
+  projectEdit.height = "25";
+
+  const projectDelete = document.createElement("img");
+  projectDelete.setAttribute("id", "project-delete");
+  projectDelete.src = "./assets/delete.svg";
+  projectDelete.width = "25";
+  projectDelete.height = "25";
+
+  projectEditDeleteDiv.appendChild(projectEdit);
+  projectEditDeleteDiv.appendChild(projectDelete);
+
+  projectItem.appendChild(projectName);
+  projectItem.appendChild(projectEditDeleteDiv);
+
+  projectList.appendChild(projectItem);
+  projectPanel.appendChild(projectList);
+}
+
+function deleteProject() {
+  projectPanel.removeChild(`${i}`);
+}
